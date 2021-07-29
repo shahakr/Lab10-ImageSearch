88251b139623b5f27ec3b8f73a53332266a2e35b
Ran Shahak <shahak@barilletsoft.com>
2021-07-29 18:39:44 +0300

    Added the form submit+callback prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
+/*
 const App = () => {
   return (
     <div className="ui container" style={{ marginTop: '20px' }}>
       <SearchBar />
     </div>
   );
-};
+};*/
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log('term ' + term);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '20px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Enter search term</label>
             <input
               type="text"
               placeholder="Search Term"
               onChange={this.onInputChange}
+              value={this.state.term}
             />
           </div>
         </form>
